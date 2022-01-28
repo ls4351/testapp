c699438b937b3bcbdebb520b4efdc2572e46672b
Lucas <ls435@duke.edu>
2022-01-27 23:43:46 -0500

    Updating server configs

--- serve_testapp.bat
+++ serve_testapp.bat
@@ -1,4 +1,4 @@
-cd C:\Users\vcm\Desktop\testapp
-git pull https://%TESTAPP_GIT_PAT%@github.com/JakeVestal/testapp.git
+cd C:\Users\vcm\production\testapp
+git pull https://github.com/ls4351/testapp.git
 venv\Scripts\python.exe -m pip install -r requirements.txt
 venv\Scripts\python.exe server.py
