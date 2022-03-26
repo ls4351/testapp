c9babbd541c2c8b3bbc1a0e742fa15face29618a
ls4351 <ls435@duke.edu>
2022-03-25 18:21:26 -0700

    Adding save_order

--- fintech_ibkr/synchronous_functions.py
+++ fintech_ibkr/synchronous_functions.py
@@ -4,7 +4,10 @@ from ibapi.common import OrderId
 from ibapi.wrapper import EWrapper
 import threading
 import time
+from datetime import datetime
+import os
 
+APP_DATA_PATH = f"{os.getenv('TESTAPP_DATA_PATH')}\submitted_orders.csv"
 default_hostname = '127.0.0.1'
 default_port = 7497
 default_client_id = 10645
@@ -22,12 +25,17 @@ class ibkr_app(EWrapper, EClient):
             'reqId', 'errorCode', 'errorString'
         ])
         self.next_valid_id = None
+        self.current_time = None
         self.historical_data_end = ''
         self.contract_details = None
         self.contract_details_end = None
         self.contract_details_reqId = None
         self.order_reqId_end = None
         self.order_reqId = None
+        self.order_status = pd.DataFrame(
+            columns=['orderId', 'status', 'filled', 'remaining', 'avgFillPrice',
+                     'permId', 'parentId', 'lastFillPrice', 'clientId',
+                     'whyHeld', 'mktCapPrice'])
 
         self.historical_data = pd.DataFrame(columns=['date', 'open', 'close', 'high', 'low'])
         self.managed_accounts = ''
@@ -57,6 +65,9 @@ class ibkr_app(EWrapper, EClient):
     def nextValidId(self, orderId: int):
         self.next_valid_id = orderId
 
+    def currentTime(self, time: int):
+        self.current_time = datetime.fromtimestamp(time).astimezone().isoformat()
+
     def historicalData(self, reqId, bar):
         record = pd.DataFrame(
             {'date': [bar.date],
@@ -84,6 +95,26 @@ class ibkr_app(EWrapper, EClient):
                     whyHeld: str, mktCapPrice: float):
         print(f'Order {orderId} status is {status}')
         self.order_reqId_end = orderId
+        self.order_status = pd.concat(
+            [
+                self.order_status,
+                pd.DataFrame({
+                    'order_id': [orderId],
+                    'status': [status],
+                    'filled': [filled],
+                    'remaining': [remaining],
+                    'avg_fill_price': [avgFillPrice],
+                    'perm_id': [permId],
+                    'parent_id': [parentId],
+                    'last_fill_price': [lastFillPrice],
+                    'client_id': [clientId],
+                    'why_held': [whyHeld],
+                    'mkt_cap_price': [mktCapPrice]
+                })
+            ],
+            ignore_index=True
+        )
+        self.order_status.drop_duplicates(inplace=True)
 
 
 def fetch_managed_accounts(hostname=default_hostname, port=default_port,
@@ -156,6 +187,35 @@ def fetch_contract_details(contract, hostname=default_hostname,
     return app.contract_details
 
 
+def save_order(contract, order, app):
+    print(f'Saving order...')
+    status = app.order_status[app.order_status['order_id'] == app.order_reqId]
+    client_id = int(status['client_id'].values[0])
+    perm_id = int(status['perm_id'].values[0])
+    lmt_price = f'{order.lmtPrice:.2f}' if order.orderType == 'LMT' else 'N/A'
+
+    data = pd.read_csv(APP_DATA_PATH)
+    data = pd.concat(
+        [data,
+         pd.DataFrame({
+             'timestamp': [app.current_time],
+             'order_id': [app.order_reqId],
+             'client_id': [client_id],
+             'perm_id': [perm_id],
+             'con_id': [0],
+             'symbol': [contract.symbol],
+             'action': [order.action],
+             'size': [order.totalQuantity],
+             'order_type': [order.orderType],
+             'lmt_price': [lmt_price]
+         })
+         ],
+        ignore_index=True
+    )
+    data.to_csv(APP_DATA_PATH, index=False)
+    print(f'Order saved!')
+
+
 def submit_order(contract, order, hostname=default_hostname,
                  port=default_port, client_id=default_client_id):
     app = ibkr_app()
@@ -172,14 +232,22 @@ def submit_order(contract, order, hostname=default_hostname,
         time.sleep(SHORT_SLEEP_SECONDS)
 
     app.order_reqId = app.next_valid_id
+    app.reqCurrentTime()
     app.placeOrder(app.order_reqId, contract, order)
 
     while (app.order_reqId_end != app.order_reqId) and (app.order_reqId_end != DEFAULT_ERROR_CODE):
+        # while not ('Submitted' in set(app.order_status['status'])):
         time.sleep(MEDIUM_SLEEP_SECONDS)
         if not app.isConnected():
             break
     app.disconnect()
 
-    msg = f'Order {app.order_reqId} successfully submitted'
+    if app.order_reqId_end == DEFAULT_ERROR_CODE:
+        msg = f'Order {app.order_reqId} did not succeed'
+    else:
+        msg = f'Order {app.order_reqId} successfully submitted'
+
+    save_order(contract, order, app)
+
     print(msg)
     return msg
